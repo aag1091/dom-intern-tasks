c0da53c0ca678d08c34d60d521122f411ba78993
Avinash Gosavi <aag1091@gmail.com>
2015-04-20 00:33:24 -0400

    Dom task stage 1 completed

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+
+  setTimeout(function(){ 
+    $('.exampleoverlay, .examplemodal').addClass('show'); 
+  }, 3000);
+
+  setTimeout(function(){ 
+    $('.exampleoverlay, .examplemodal').removeClass('show'); 
+  }, 8000);
+
+});
